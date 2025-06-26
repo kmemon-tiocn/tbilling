dff7af309b4a8f5d91c30877b50eeddadeb54228
mehedi07 <mehedi@ticonsys.com>
2025-06-26 16:12:14 +0600

    dashboard added

--- main/views/dashboard.py
+++ main/views/dashboard.py
@@ -4,3 +4,17 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def dashboard_view(request):
     return render(request, 'dashboard.html')
+
+
+
+
+
+##Temporary Code 
+
+@login_required
+def project_table(request):
+    return render(request, 'project-table.html')
+
+@login_required
+def client_list(request):
+    return render(request, 'client-table.html')
